bf38cb79f57dc1a435cd0f70c331d429fe16c595
Admin <admin@Deepthought-2.local>
2021-09-27 23:26:58 +0100

    navigation-bar responsiveness

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {useState} from 'react';
+
 import {Link} from 'react-router-dom';
 import './NavigationBar.css';
 
 export default function NavigationBar() {
 
+    const [sidebar, setSidebar] = useState(false);
+
+    const showSidebar = () => setSidebar(!sidebar);
+
     return (
         <div className="navigation-bar">
             <h1><Link to="/" className="home">Boladek.</Link></h1>
-            <ul>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-                <li><Link to="/resume">Resume</Link></li>
+            <ul className="bigger-screen">
+                <li className="big-screen"><Link to="/about">About</Link></li>
+                <li className="big-screen"><Link to="/contact">Contact</Link></li>
+                <li className="big-screen"><Link to="/resume">Resume</Link></li>
             </ul>
+            <div className="font-icon">
+                <FontAwesomeIcon icon={faBars} onClick={showSidebar} />
+                <nav className={sidebar ? 'nav-menu active' : 'nav-menu-close'}>
+                <ul className="nav-menu-items">
+                    <li className="small-screen"><Link to="/">Close</Link></li>
+                    <li className="small-screen"><Link to="/about">About</Link></li>
+                    <li className="small-screen"><Link to="/contact">Contact</Link></li>
+                    <li className="small-screen"><Link to="/resume">Resume</Link></li>
+                </ul>
+            </nav>
+            </div>
         </div>
     )
 }
--- src/pages/Resume/Resume.tsx
+++ src/pages/Resume/Resume.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import Footer from '../../components/Footer/Footer';
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
 import Pdf from '../../components/Pdf/Pdf';
 
 export default function Resume() {
     return (
-        <div>
+        <div className="resume-container">
             <NavigationBar />
             <Pdf />
+            <Footer />
         </div>
     )
 }
